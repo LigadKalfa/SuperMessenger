b407d69c08347428035333807699742b3958e8bd
admin <admin@admins-mac-mini>
2019-01-01 19:30:57 +0200

    navigation bar

---
+++ SuperMessenger/Controllers/ChatsController.swift
@@ -0,0 +1,32 @@
+//
+//  ChatsController.swift
+//  SuperMessenger
+//
+//  Created by admin on 01/01/2019.
+//  Copyright © 2019 LigadKalfa&DanielPichhadze. All rights reserved.
+//
+
+import UIKit
+
+class ChatsController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
