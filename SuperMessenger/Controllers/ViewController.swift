54d9dcfdddbe9a9cd248d327a7002258470a6549
Ligad Kalfa <lmkntl@gmail.com>
2019-01-01 19:18:06 +0200

    abstract registration

--- SuperMessenger/Controllers/ViewController.swift
+++ SuperMessenger/Controllers/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseUI
 
 class ViewController: UIViewController {
     //Mark: Properties
@@ -49,5 +50,29 @@ class ViewController: UIViewController {
         
         view.sendSubviewToBack(backgroundImageView)
     }
+    
+    @IBAction func loginTapped(_ sender: SimpButton) {
+        let authUI = FUIAuth.defaultAuthUI()
+        
+        guard authUI != nil else {
+            return
+        }
+        
+        authUI?.delegate = self
+        
+        // The var which we gonna design as the auth view controller
+        let authViewController = authUI!.authViewController()
+        present(authViewController, animated: true, completion: nil)
+    }
 }
 
+extension ViewController: FUIAuthDelegate{
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
+        guard error == nil else{
+            return
+        }
+        
+        performSegue(withIdentifier: "goHome", sender: self)
+    }
+}
