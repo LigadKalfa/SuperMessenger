9903ae18a403408f0ab0f1a707ffabfb6eb533ab
Ligad Kalfa <lmkntl@gmail.com>
2019-03-14 01:59:05 +0200

    commit irrelevant files

--- SuperMessenger/Controllers/ExtensionAllChatsController.swift
+++ SuperMessenger/Controllers/ExtensionAllChatsController.swift
@@ -17,14 +17,14 @@ extension AllChatsController{
         clearData()
         
         let delegate = UIApplication.shared.delegate as? AppDelegate
-        
+        /*
         if let context = delegate?.persistentContainer.viewContext {
-            
+            /*
             let boy = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             boy.name = "Boyfriend Of Bijo"
             boy.profileImageName = "boy.jpeg"
 
-            createMessageWithText(text: "Bijo is my sweet little lover", friend: boy, minutesAgo: 2, context: context)
+            AllChatsController.createMessageWithText(text: "Bijo is my sweet little lover", friend: boy, minutesAgo: 2, context: context)
             
             createGirlMessagesWithContext(context: context)
             
@@ -32,20 +32,20 @@ extension AllChatsController{
             bijo.name = "Bijo Himself"
             bijo.profileImageName = "bijo.jpeg"
             
-            createMessageWithText(text: "Give me Hachapuri!", friend: bijo, minutesAgo: 10, context: context)
+            AllChatsController.createMessageWithText(text: "Give me Hachapuri!", friend: bijo, minutesAgo: 10, context: context)
             
             let mama = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             mama.name = "Mamaliga"
             mama.profileImageName = "Mama.jpeg"
 
-            createMessageWithText(text: "Oh sweet son!", friend: mama, minutesAgo: 60 * 24, context: context)
+            AllChatsController.createMessageWithText(text: "Oh sweet son!", friend: mama, minutesAgo: 60 * 24, context: context)
 
             let pops = NSEntityDescription.insertNewObject(forEntityName: "Friend", into: context) as! Friend
             pops.name = "Don Pepe"
             pops.profileImageName = "Pops.jpeg"
             
-            createMessageWithText(text: "Oh sweet son!", friend: pops, minutesAgo: 8 * 60 * 24, context: context)
-
+            AllChatsController.createMessageWithText(text: "Oh sweet son!", friend: pops, minutesAgo: 8 * 60 * 24, context: context)
+*/
             
             do {
                 try(context.save())
@@ -53,7 +53,7 @@ extension AllChatsController{
                 print(err)
             }
         }
-        
+        */
         loadData()
     }
     
@@ -62,23 +62,25 @@ extension AllChatsController{
         girl.name = "Bijo's Girlfriend"
         girl.profileImageName = "someProfile.jpeg"
         
-        createMessageWithText(text: "Hello, my name is Bijo's girl, wanna dance tonight?? <3 i have a" +
+        AllChatsController.createMessageWithText(text: "Hello, my name is Bijo's girl, wanna dance tonight?? <3 i have a" +
             "lot to tell you about how my day was going and how every body love me at work!", friend: girl, minutesAgo: 3, context: context)
-        createMessageWithText(text: "and tonight? -_-", friend: girl, minutesAgo: 2, context: context)
-        createMessageWithText(text: "seriosly i will always love bijo bijoshvily", friend: girl, minutesAgo: 0, context: context)
-        createMessageWithText(text: "Do you love me do you do you?", friend: girl, minutesAgo: 0, context: context)
+        AllChatsController.createMessageWithText(text: "and tonight? -_-", friend: girl, minutesAgo: 2, context: context)
+        AllChatsController.createMessageWithText(text: "seriosly i will always love bijo bijoshvily", friend: girl, minutesAgo: 0, context: context)
+        AllChatsController.createMessageWithText(text: "Do you love me do you do you?", friend: girl, minutesAgo: 0, context: context)
 
         //response message
-        createMessageWithText(text: "Well, dear, im in love with you.", friend: girl, minutesAgo: 0, context: context, isSender: true)
+        AllChatsController.createMessageWithText(text: "Well, dear, im in love with you.", friend: girl, minutesAgo: 0, context: context, isSender: true)
     }
     
-    private func createMessageWithText(text: String, friend: Friend, minutesAgo: Double,context: NSManagedObjectContext,
-                                       isSender: Bool = false) {
+    static func createMessageWithText(text: String, friend: Friend, minutesAgo: Double,context: NSManagedObjectContext,
+                                       isSender: Bool = false) -> Message {
         let message = NSEntityDescription.insertNewObject(forEntityName: "Message", into: context) as! Message
         message.friend = friend
         message.text = text
         message.date = NSDate().addingTimeInterval(-minutesAgo * 60) as Date
         message.isSender = isSender
+        
+        return message
     }
     
     func loadData() {
@@ -89,7 +91,7 @@ extension AllChatsController{
             
             messages = [Message]()
             
-            if let friends = fetchFriends() {
+            if let friends = AllChatsController.fetchFriends() {
                 for friend in friends {
                         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Message")
                         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
@@ -110,7 +112,7 @@ extension AllChatsController{
         }
     }
     
-    private func fetchFriends() -> [Friend]? {
+    static func fetchFriends() -> [Friend]? {
         let delegate = UIApplication.shared.delegate as? AppDelegate
         
         if let context = delegate?.persistentContainer.viewContext {
